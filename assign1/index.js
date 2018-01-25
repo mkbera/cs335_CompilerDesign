87d03950c7aaae8f08e73d50e891883c5aad1b30
Gurpreet Singh <g.singh6343@gmail.com>
2018-01-25 15:37:43 +0530

    Cleaned

--- assign1/index.js
+++ assign1/index.js
@@ -2,51 +2,23 @@ var MyLexer = require('genlexer');
 var lexer = new MyLexer();
 
 
-var dict = {
-    boolean: 0,
-    break: 0,
-    byte: 0,
-    case: 0,
-    char: 0,
-    class: 0,
-    const: 0,
-    continue: 0,
-    default: 0,
-    do: 0,
-    double: 0,
-    else: 0,
-    extends: 0,
-    float: 0,
-    for: 0,
-    if: 0,
-    import: 0,
-    instanceof: 0,
-    int: 0,
-    long: 0,
-    new: 0,
-    return: 0,
-    short: 0,
-    static: 0,
-    super: 0,
-    switch: 0,
-    this: 0,
-    void: 0,
-    while: 0,
-    integer: 0,
-    float: 0
-};
-
-
 function runLexer(lexer) {
+    var tokens = {};
     var firstToken = lexer.nextToken();
 
     while (!lexer.isEOF(firstToken)) {
-        //if (!(firstToken.name in dict)) {
-            //console.log("\t>> ERROR:\t" + firstToken.value + " is not a valid syntax");
-            //return;
-        //}
+        if (firstToken.name == "invalid") {
+            console.log(">> ERROR:" + firstToken.value + " is not a valid syntax");
+            return;
+        }
+
+        if (!(firstToken.name in tokens)) {
+            tokens[firstToken.name] = 0;
+        }
+        tokens[firstToken.name] += 1;
+
         console.log(firstToken);
-		console.log("");
+        console.log("");
         firstToken = lexer.nextToken();
     }
 
