e0aa2a4b055a21d43a6270b3cadcb3a63990212d
prannb <prann.bansal@gmail.com>
2018-04-02 21:45:53 +0530

    more tests,

---
+++ assign4/test/array_func.java
@@ -0,0 +1,19 @@
+import IO;
+
+class rec {
+	public int[5][5] array_func(int[5][5] a, int b) {
+		for (int i=0; i<5; i++){
+			for (int j=0; j<5; j++){
+				a:[i][j] = b;
+			}
+		}
+		return a;
+	}
+
+	public void main() {
+		int[5][5] a;
+		int b = 9;
+		a = array_func(a, b);
+		// int c = gcd(a, b);
+	}
+}
