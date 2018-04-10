163b1fa8d770be08384b1969521846c3de7a7a77
prannb <prann.bansal@gmail.com>
2018-04-10 20:57:40 +0530

    just testing

--- assign4/test/test1.java
+++ assign4/test/test1.java
@@ -1,48 +1,49 @@
 import IO;
 
 class test {
-	public int x = 10;
-	int a, c = 5.012;
-	int[2][5] d;
+	// public int x = 10;
+	// int a, c = 5.012;
+	// int[2][5] d;
 
-	float[2] test = {12, 4};
+	// float[2] test = {12, 4};
 
-	int function_name(int x, int y) {
-		float b;
-		x = b;
-		return 5;
-	}
+	// int function_name(int x, int y) {
+	// 	float b;
+	// 	x = b;
+	// 	return 5;
+	// }
 
-	int[2][5] test2(int[2][5] arr) {
-		int r = (float) x + 1.020 + a;
-		return arr;
-	}
+	// int[2][5] test2(int[2][5] arr) {
+	// 	int r = (float) x + 1.020 + a;
+	// 	return arr;
+	// }
 
-	public int test_func(int[2][5] arr) {
-		int a = 1;
-		int b = 0;
+	// public int test_func(int[2][5] arr) {
+	// 	int a = 1;
+	// 	int b = 0;
 
-		boolean c = true;
+	// 	boolean c = true;
 
-		d = test2(arr);
+	// 	d = test2(arr);
 
-		a = b + 5;
-		a = -b;
+	// 	a = b + 5;
+	// 	a = -b;
 
-		d:[-1][4] = 0.0;
+	// 	d:[-1][4] = 0.0;
 
-		for (int i = 0; i = 5; ++i) {
-			for (int j = 0; j = 5; ++j) {
-				d:[i][j] = 1 + b;
-				return 1;
-			}
-		}
+	// 	for (int i = 0; i = 5; ++i) {
+	// 		for (int j = 0; j = 5; ++j) {
+	// 			d:[i][j] = 1 + b;
+	// 			return 1;
+	// 		}
+	// 	}
 
-		return a;
-	}
+	// 	return a;
+	// }
 
 	public void main() {
-		int a = test_func(d);
-		int b = function_name(1, 2);
+		int a = 2.0;
+		// float b = 3;
+		// b = a;
 	}
 }
