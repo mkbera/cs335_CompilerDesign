39a84cb5d92b24b60a75789d6cfcd33cb2f01196
Gurpreet Singh <g.singh6343@gmail.com>
2018-04-18 23:49:35 +0530

    Removed char from symbol-table

--- integrated/test/irgen/simple.java
+++ integrated/test/irgen/simple.java
@@ -1,21 +1,11 @@
 import IO;
 
 class rec {
-	int a = 9;
-	int b = 10;
-	
-
 	public void main() {
-		// IO io = new IO();
-		rec[5] arr;
-		for (int i=0; i<5; i++){
-			arr:[i] = new rec();
-		}
-		arr:[1].a = 9;
-		
-		// for (int i=0; i<5; i++){
-		// 	io.print_int(arr:[i].a);
-		// io.print_int(arr:[1].b);
-		// }
+		IO io = new IO();
+		char a = io.scan_char();
+		char b = io.scan_char();
+		io.print_char(a+b);
+
 	}
 }
